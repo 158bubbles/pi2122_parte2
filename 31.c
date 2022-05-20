47a468a7e849127b1f8f2d014834e601eaaa3e20
158bubbles <49231263+158bubbles@users.noreply.github.com>
2022-05-20 18:11:10 +0100

    Create 31.c

---
+++ 31.c
@@ -0,0 +1,31 @@
+void insereNaLInt(int v, LInt *l){
+    LInt current = *l;
+    LInt nodo = newLInt(v, NULL);
+    
+    if(*l == NULL){
+        *l = nodo;
+        return;
+    } else {
+        
+        while(current->prox != NULL){
+            current = current->prox;
+        }
+        
+        current->prox = nodo;
+        nodo->prox = NULL;
+    }
+}
+
+
+
+void inorder (ABin a, LInt * l) {
+    
+    if(a == NULL){
+        return;
+        
+    } else {
+        inorder(a->esq, l);
+        insereNaLInt(a->valor, l);   //vai inserir o nodo da root na LInt que ja tem os elementos da esq
+        inorder(a->dir, l);
+    }
+}
