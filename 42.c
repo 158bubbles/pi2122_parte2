277c46a106b828a41a41272548afbcd738cae43f
158bubbles <49231263+158bubbles@users.noreply.github.com>
2022-05-23 00:32:13 +0100

    Create 42.c

---
+++ 42.c
@@ -0,0 +1,12 @@
+int contaFolhas (ABin a) {
+    
+    if(a == NULL){
+        return 0;
+    } 
+    else if(a->esq == NULL && a->dir == NULL){
+        return 1;
+    }
+    else {
+        return contaFolhas(a->esq) + contaFolhas(a->dir);
+    }
+}
